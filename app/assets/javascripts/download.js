ac0ad66ee1709ec38bed4f2cea08180dfa0cb338
Assaf Shomer <coincooler@gmail.com>
2015-04-16 11:11:17 +0300

    Added flash animation on download pages

--- app/assets/javascripts/download.js
+++ app/assets/javascripts/download.js
@@ -1,15 +1,31 @@
 function clearFlashes() {
-	$('.alert-success').hide();$('.alert-error').hide();$('.alert-info').hide();
+	$('.alert-success').hide();
+	$('.alert-error').hide();
+	$('.alert-info').hide();
+	flasher = 0;
 }
 function popFlash(id, text) {
-	$('#'+id).bind('ajax:complete', function() {
-		clearFlashes();
+	$('#'+id).bind('ajax:complete', function() {		
 		if (document.cookie.match(/no_usb/)) {
-			$('.alert:contains("No USB storage detected")').show().delay(slow).fadeOut(fade);	
+			clearFlashes();
+			$('.alert:contains("No USB storage detected")').show();	
 		} else if (document.cookie.match(/missing_files/)){
 			window.location.replace("/");				
 		}	else {
-			$('.alert:contains("'+text+'")').show().delay(slow).fadeOut(fade);	
+			animateFlash($('.alert:contains("'+text+'")'));
 		};
 	});
+}
+
+function animateFlash(element){
+  setTimeout(function() {		  	
+  	alert = element;
+  	width = alert.text().length*(7.5);
+  	color = alert.css('color');
+  	bgcolor = alert.css('background-color');
+  	clearFlashes();
+		alert.show();
+		alert.animate({ backgroundColor: '#8ECDA6', color: 'black', width: 600 }, 2000);
+		alert.animate({ backgroundColor: bgcolor, color: color, width: width }, 2000);
+  }, 1000);
 }
