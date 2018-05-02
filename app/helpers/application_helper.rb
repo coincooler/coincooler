36a20a43c931d37c3f19ccf763e50e1ae0b8480f
Assaf Shomer <assafshomer@gmail.com>
2018-05-02 17:10:17 +0300

    better pi detection, remove Fixnum reference

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
-  def osx?
+  def self.osx?
     /darwin/ =~ RUBY_PLATFORM
+  end
+
+  def self.pi?
+    begin
+      system("cat /etc/rpi-issue", err: File::NULL) =~ /Raspberry Pi/ ||
+      system("cat /etc/os-release", err: File::NULL) =~ /Raspbian/ ? true : false
+    rescue => e
+      Rails.logger.debug "got #{e.message} when tried to check if PI"
+      false
+    end
   end
 end
