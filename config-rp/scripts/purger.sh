f79a53c9aa28d589778c5436934d6aed380c0788
Assaf Shomer <assafshomer@gmail.com>
2018-05-06 16:28:49 +0300

    no need to srm pid

--- config-rp/scripts/purger.sh
+++ config-rp/scripts/purger.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 find /home/pi/coincooler/files -iname "*.*" -exec srm {} \;
-find /home/pi/coincooler/tmp/pids -iname "*.*" -exec srm {} \;
+find /home/pi/coincooler/tmp/pids -iname "*.*" -exec rm {} \;
 find /home/pi/coincooler/log -iname "*.*" -exec srm {} \;
 find /home/pi/coincooler/files/uploads/ -type f -exec srm {} \;
 find /home/pi/coincooler/files/uploads/ -mindepth 1 -depth -type d -exec rmdir {} \;
